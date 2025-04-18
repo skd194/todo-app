98a1f2eb8b46bfa0d112db68e610b9880d29d34e
sks <shyamkumarsb007@gmail.com>
2025-04-18 21:50:23 +0530

    added a  dropdown for the input

--- src/modules/todo/components/todo-list.ts
+++ src/modules/todo/components/todo-list.ts
@@ -1,18 +1,46 @@
-import { customElement, bindable } from "aurelia-framework"
-import { Todo } from '../models/todo'
+import { bindable } from "aurelia-framework";
+import { Todo } from "../models/todo";
 
-@customElement("todo-list")
-export class TodoListComponent {
-  @bindable public todos: Todo[]
-  @bindable public onRemove: (todo: Todo) => void
-  @bindable public onAdd: (todo: Todo) => void
+export class TodoList {
+  @bindable public todos: Todo[];
+  @bindable public onRemove: (todo: Todo) => void;
+  @bindable public onAdd: (todo: Todo) => void;
 
-  public newTodoDescription: string;
+  public newTodoDescription: string = "";
+  public showDropdown: boolean = false;
+  public suggestions: string[] = [
+    "Buy groceries",
+    "Walk the dog",
+    "Read a book",
+    "Write a blog post",
+    "Go to the gym",
+    "Plan a trip",
+  ];
+  public filteredSuggestions: string[] = [];
+
+  public onInputChange(): void {
+    if (this.newTodoDescription.trim() === "") {
+      this.showDropdown = false;
+      this.filteredSuggestions = [];
+      return;
+    }
+
+    this.filteredSuggestions = this.suggestions.filter((suggestion) =>
+      suggestion.toLowerCase().includes(this.newTodoDescription.toLowerCase())
+    );
+    this.showDropdown = this.filteredSuggestions.length > 0;
+  }
+
+  public selectSuggestion(suggestion: string): void {
+    this.newTodoDescription = suggestion;
+    this.showDropdown = false;
+  }
 
   public addTodo(): void {
     if (this.newTodoDescription) {
       const todo: Todo = Todo.new(this.newTodoDescription);
       this.newTodoDescription = "";
+      this.showDropdown = false;
       if (this.onAdd) {
         this.onAdd(todo);
       }
@@ -24,5 +52,4 @@ export class TodoListComponent {
       this.onRemove(todo);
     }
   }
-
 }
